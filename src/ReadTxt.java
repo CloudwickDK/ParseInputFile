ac97c4a6390fb9b0c2cc2ad6a25e6e180e525718
Despoina Karna <falsedk@hotmail.com>
2016-03-09 00:09:53 +0000

    count frequency with arralist

--- src/ReadTxt.java
+++ src/ReadTxt.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.Iterator;
+//import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.ListIterator;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.stream.Stream;
 
 public class ReadTxt {
@@ -28,7 +30,9 @@ public class ReadTxt {
 			}
 			FileWriter fw = new FileWriter(file);
 			FileWriter fw2 = new FileWriter(file2);
-			HashMap<String, Integer> hm = new HashMap<String, Integer>();
+			// HashMap<String, Integer> hm = new HashMap<String, Integer>();
+			ArrayList<MyStruct> list = new ArrayList<MyStruct>();
+
 			br = new BufferedReader(new FileReader("./src/pg2600.txt"));
 			bw = new BufferedWriter(fw);
 			bw2 = new BufferedWriter(fw2);
@@ -40,28 +44,42 @@ public class ReadTxt {
 					if (word.isEmpty()) {
 						continue;
 					}
-					if (hm.containsKey(word)) {
-						hm.put(word, hm.get(word) + 1);
+					MyStruct word_frequency = new ReadTxt().new MyStruct(word,0); // initiate object for this word
+					ListIterator listIterator = list.listIterator();
+					while (listIterator.hasNext()){
+
+				         MyStruct element = (MyStruct)listIterator.next();
+				         if(word == element.word){
+				        	 word_frequency.setCount(element.count);
+				        	 list.add(word_frequency);
+				         }else{
+				        	 word_frequency.setCount(1);
+				         }
+					}/*
+					if (list.contains(word_frequency)) {
+						word_frequency.setCount(word_frequency.count);
+
 					} else {
-						hm.put(word, 1);
+						word_frequency.setCount(word_frequency.count);
 					}
+					list.add(word_frequency); */
 				}
 			}// while
+
 			bw.write("word, frequency\n");
-			for (String key : hm.keySet()) { // key corresponds to each word
-				bw.write(key + ", " + hm.get(key) + "\n");
-			}
 
-			Map<String, Integer> lhm = sortByValue(hm);
-			bw2.write("word, frequency\n");
-			int loopcount = 0;
-			for (String key : lhm.keySet()) { // key corresponds to each word
-				if (loopcount++ > 10)
-					break; // print only the first 10
-				bw2.write(key + ", " + lhm.get(key) + "\n");
-				System.out.println(key + ", " + lhm.get(key) + "\n");
+			for (MyStruct object : list) { // key corresponds to each word
+				bw.write(object.word + ", " + object.count + "\n");
 			}
-
+			System.out.println("hey");
+			/*
+			 * Map<String, Integer> lhm = sortByValue(hm);
+			 * bw2.write("word, frequency\n"); int loopcount = 0; for (String
+			 * key : lhm.keySet()) { // key corresponds to each word if
+			 * (loopcount++ > 10) break; // print only the first 10
+			 * bw2.write(key + ", " + lhm.get(key) + "\n");
+			 * System.out.println(key + ", " + lhm.get(key) + "\n"); }
+			 */
 		} catch (IOException e) {
 			System.out.println(e);
 		} finally {
@@ -92,4 +110,36 @@ public class ReadTxt {
 				e -> result.put(e.getKey(), e.getValue()));
 		return result;
 	}
+
+	public class MyStruct {
+		String word;
+		int count;
+
+		public MyStruct(String word, int count) {
+			this.word = word;
+			this.count = count;
+		}
+
+		public void setCount(int count) {
+			this.count = count;
+		}
+
+		@Override
+		public boolean equals(Object object) {
+			boolean isEqual = false;
+
+			if (object != null && object instanceof MyStruct) {
+				isEqual = (this.word == ((MyStruct) object).word);
+			}
+
+			return isEqual;
+		}
+
+		@Override
+		public int hashCode() {
+			//return Object.hashCode(word);
+			return 0;
+		}
+
+	}// MyStruct
 }
